6b62507673c4a58f9a43313411b4070fc8ce253e
王瑞玉 <wangruiyu@bjknrt.com>
2023-05-22 11:43:01 +0800

    style(middle): 增加中间件相关接口的代码注释

--- middleware/handler.go
+++ middleware/handler.go
@@ -2,12 +2,12 @@ package middleware
 
 import "net/http"
 
-// Handler 中间价接口
+// Handler 中间件处理器接口
 type Handler interface {
 	Handle(ctx *Context, writer http.ResponseWriter, request *http.Request) error
 }
 
-// HandlerFunc 中间价接口方法的实现
+// HandlerFunc 中间件处理器接口的方法类型的实现
 type HandlerFunc func(ctx *Context, writer http.ResponseWriter, request *http.Request) error
 
 func (m HandlerFunc) Handle(ctx *Context, writer http.ResponseWriter, request *http.Request) error {
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,23 +5,33 @@ import (
 	"net/http"
 )
 
+// NewMiddleware 中间件的构造函数
 func NewMiddleware() Middleware {
 	return Middleware{
 		ctx:                  &Context{},
 		registeredMiddleware: make(map[string]Handler),
+		middlewareKeys:       make([]string, 0),
 	}
 
 }
 
+// Middleware 中间件
 type Middleware struct {
-	ctx                  *Context
+	//上下文环境
+	ctx *Context
+	//注册的中间件
 	registeredMiddleware map[string]Handler
+	//有序的中间件key
+	middlewareKeys []string
 }
 
+// Handle 按照`middlewareKeys`中的顺序执行中间件
+// write: 响应
+// request: 请求
 func (m *Middleware) Handle(write http.ResponseWriter, request *http.Request) error {
 
-	for _, value := range m.registeredMiddleware {
-		err := value.Handle(m.ctx, write, request)
+	for _, value := range m.middlewareKeys {
+		err := m.registeredMiddleware[value].Handle(m.ctx, write, request)
 		if err != nil {
 			return err
 		}
@@ -29,15 +39,22 @@ func (m *Middleware) Handle(write http.ResponseWriter, request *http.Request) er
 	return nil
 }
 
+// RegisterHandler 注册中间件(接口实现)
+// id: 中间件的key
+// handler: 中间件
 func (m *Middleware) RegisterHandler(id string, handler Handler) error {
 	_, exist := m.registeredMiddleware[id]
 	if exist == false {
 		m.registeredMiddleware[id] = handler
+		m.middlewareKeys = append(m.middlewareKeys, id)
 		return nil
 	}
 	return RepeatRegisterErr
 }
 
+// RegisterHandlerFunc 注册中间件（方法实现）
+// id: 中间件的key
+// handler: 中间件
 func (m *Middleware) RegisterHandlerFunc(id string, handlerFunc HandlerFunc) error {
 	return m.RegisterHandler(id, handlerFunc)
 }
