3bb187b6e7ed5e0cc6d763295695e4a5bbb5a04c
王瑞玉 <wangruiyu@bjknrt.com>
2023-06-19 16:08:55 +0800

    feature(matcher): 增加方法和主机匹配器

---
+++ matcher/host/host.go
@@ -0,0 +1,26 @@
+package host
+
+import (
+	"log"
+	osPath "path"
+	"strings"
+)
+
+func Match(path, patterns string) bool {
+	if len(patterns) == 0 || patterns == "*" {
+		return true
+	}
+	for _, pattern := range strings.Split(patterns, ",") {
+		if mathPath(path, pattern) {
+			return true
+		}
+	}
+	return false
+}
+func mathPath(path, pattern string) bool {
+	match, err := osPath.Match(pattern, path)
+	if err != nil {
+		log.Printf("match path error path：%v, pattern: %v", path, pattern)
+	}
+	return match
+}
