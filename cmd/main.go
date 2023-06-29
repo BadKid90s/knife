cff5e95d4d3d29931c9ba560c22d2701348fc849
王瑞玉 <wangruiyu@bjknrt.com>
2023-06-29 18:03:31 +0800

    refactor(main): 简化启动方法

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"gateway/core"
-	"gateway/network"
 	"log"
 )
 
 func main() {
 	var configFile = "conf/application.yaml"
-	listener, err := network.NewListenTCP(":8090")
-	if err != nil {
-		log.Fatalf("app listen err")
-	}
 
-	app := core.NewApp(listener, configFile)
-	err = app.Start()
-	if err != nil {
-		log.Printf("app runing err")
-	}
+	app := core.NewApp(configFile)
 
-	fmt.Println("gateway hello word")
+	err := app.Start()
+	if err != nil {
+		log.Printf("app runing err \n")
+	}
 }
