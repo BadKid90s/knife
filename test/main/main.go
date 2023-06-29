39c533b76c3e5b514998202b82ff70505d3d7920
王瑞玉 <wangruiyu@bjknrt.com>
2023-06-29 11:00:10 +0800

    refactor(main): 修改测试方法

--- test/main/main.go
+++ test/main/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"gateway/config"
 	"gateway/core"
 	"gateway/network"
+	"gateway/route"
 	"log"
 )
 
 func main() {
-
+	var configFile = "conf/application.yaml"
 	listener, err := network.NewListenTCP(":8090")
 	if err != nil {
 		log.Fatalf("app listen err")
 	}
 
-	err = config.ParsePredicateConfig()
+	err = route.ParseRouteConfig(configFile)
 	if err != nil {
 		log.Fatalf("config file parse err")
 	}
