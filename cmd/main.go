5cb1f2c023c89607c21e9590adcc3198f51ff1b0
王瑞玉 <wangruiyu@bjknrt.com>
2023-06-30 09:47:36 +0800

    refactor(main): 增加启动的Banner

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"gateway/core"
+	"gateway/util"
 	"log"
 )
 
 func main() {
+	printBanner()
+
 	var configFile = "conf/application.yaml"
 
 	app := core.NewApp(configFile)
 
 	err := app.Start()
 	if err != nil {
-		log.Printf("app runing err \n")
+		log.Printf("app runing err %s \n", err)
 	}
+}
+
+func printBanner() {
+	bytes, err := util.ReadConfigFile("conf/banner.txt")
+	if err != nil {
+		log.Fatalf("loading programe banner err %s \n", err)
+	}
+	println(string(bytes))
 }
