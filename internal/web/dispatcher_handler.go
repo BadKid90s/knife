53c2a2073ba1000a0bc68528b12bcadcb258690d
王瑞玉 <wangruiyu@bjknrt.com>
2023-07-05 11:05:22 +0800

    refactor(main): 增加 DispatcherHandler 的日志

--- internal/web/dispatcher_handler.go
+++ internal/web/dispatcher_handler.go
@@ -25,6 +25,7 @@ func (h *DispatcherHandler) AddHandler(mapping HandlerMapping) {
 // write: 响应
 // request: 请求
 func (h *DispatcherHandler) ServeHTTP(write http.ResponseWriter, request *http.Request) {
+	log.Printf("dispatcher handler received request. url [%s] \n", request.URL)
 	exchange := NewServerWebExchange(write, request)
 	for _, handlerMapping := range h.handlerMappings {
 		handler, err := handlerMapping.GetHandler(exchange)
