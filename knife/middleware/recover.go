ec111b27d09951fe98c90d70abee1d045b582e88
wangruiyu <wry10150@163.com>
2024-01-26 13:14:40 +0800

    perf(main): 修正警告消息

--- knife/middleware/recover.go
+++ knife/middleware/recover.go
@@ -3,9 +3,9 @@ package middleware
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"knife"
 	"net/http"
+	"os"
 	"runtime"
 )
 
@@ -44,21 +44,21 @@ func stack(skip int) []byte {
 			break
 		}
 		// Print this much at least.  If we can't find the source, it won't show.
-		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		_, _ = fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
 		if file != lastFile {
-			data, err := ioutil.ReadFile(file)
+			data, err := os.ReadFile(file)
 			if err != nil {
 				continue
 			}
 			lines = bytes.Split(data, []byte{'\n'})
 			lastFile = file
 		}
-		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+		_, _ = fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
 	}
 	return buf.Bytes()
 }
 
-// source returns a space-trimmed slice of the n'th line.
+// source returns a space-trimmed slice of the nth line.
 func source(lines [][]byte, n int) []byte {
 	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
 	if n < 0 || n >= len(lines) {
@@ -77,13 +77,13 @@ func function(pc uintptr) []byte {
 	// The name includes the path name to the package, which is unnecessary
 	// since the file name is already included.  Plus, it has center dots.
 	// That is, we see
-	//	runtime/debug.*T·ptrmethod
+	//	runtime/debug.*T·ptr method
 	// and want
-	//	*T.ptrmethod
+	//	*T.ptr method
 	// Also the package path might contains dot (e.g. code.google.com/...),
 	// so first eliminate the path prefix
-	if lastslash := bytes.LastIndex(name, slash); lastslash >= 0 {
-		name = name[lastslash+1:]
+	if lastSlash := bytes.LastIndex(name, slash); lastSlash >= 0 {
+		name = name[lastSlash+1:]
 	}
 	if period := bytes.Index(name, dot); period >= 0 {
 		name = name[period+1:]
