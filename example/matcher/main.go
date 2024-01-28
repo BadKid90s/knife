ac9c90f19e49cdcf421d06768ccba1c1d424429a
wangruiyu <wry10150@163.com>
2024-01-28 17:55:08 +0800

    feat(main): 修改示例代码

--- example/matcher/main.go
+++ example/matcher/main.go
@@ -9,14 +9,9 @@ import (
 )
 
 func main() {
-	//创建http路由
-	mux := http.NewServeMux()
-	mux.HandleFunc("/a", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = writer.Write([]byte("hello 8080 a "))
-	})
 
 	//创建中间件链
-	chain := knife.NewChain(mux)
+	chain := knife.NewChain()
 	//添加日志记录中间件
 	chain.Use(middleware.Logger())
 	//添加错误处理中间件
