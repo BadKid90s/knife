5e845ddad351f9634292dbfe36f34db60c31f73e
wangruiyu <wry10150@163.com>
2024-01-29 16:12:14 +0800

    fix(main): 修复日志记录时间不对的BUG

--- knife/logger.go
+++ knife/logger.go
@@ -3,6 +3,19 @@ package knife
 import (
 	"log"
 	"os"
+	"time"
 )
 
-var Logger = log.New(os.Stdout, "", log.LstdFlags)
+var Logger = newLogger()
+
+type loggers struct {
+	*log.Logger
+}
+
+func newLogger() *loggers {
+	now := time.Now().Format("2006-01-02 15:04:05.000")
+	l := log.New(os.Stdout, now+" ", log.Lshortfile)
+	return &loggers{
+		Logger: l,
+	}
+}
